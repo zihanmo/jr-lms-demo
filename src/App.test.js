e76299d78d9a417159586323650cc5edaf74693f
Joey Zheng <joey.z.rp@gmail.com>
2020-02-07 20:42:01 +1000

    Added test

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StaticRouter } from 'react-router-dom';
+
+import App from './App';
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render((
+		<StaticRouter>
+			<App />
+		</StaticRouter>
+	), div);
+	ReactDOM.unmountComponentAtNode(div);
+});
