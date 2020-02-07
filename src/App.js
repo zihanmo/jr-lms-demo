263954bf57f41e723dedde37a8efc44d935470bf
Joey Zheng <joey.z.rp@gmail.com>
2020-02-07 19:34:58 +1000

    starting point

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
-import Routes from './routes/Routes';
-import TopNav from './navigation/TopNav';
-
 function App() {
 	return (
 		<div>
-			<TopNav />
-			<main className="container">
-				<Routes />
-			</main>
+			Content goes here!!!
 		</div>
 	);
 }
--- src/routes/URLMap.js
+++
@@ -1,4 +0,0 @@
-export const COURSE_BASE_URL = '/courses';
-export const STUDENT_BASE_URL = '/students';
-export const LECTURER_BASE_URL = '/lecturers';
-export const LOGIN_URL = '/login';
